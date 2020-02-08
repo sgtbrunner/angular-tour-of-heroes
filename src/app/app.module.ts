b983ce27d90c8bd4046f4f64ef1b983e91bc4603
Web Developer <guilherme.brunner@gmail.com>
2020-02-08 15:19:50 -0300

    master: end of part II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../models/hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+heroes: Hero[] = HEROES;
+selectedHero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
